bc0362ac489d0a5796f599f4767779ea5be0ae09
sodastereo <f_er_@hotmail.com>
2019-07-08 23:24:42 -0400

    contact form added

--- app.js
+++ app.js
@@ -1,35 +1,102 @@
 
-  // Get the modal
-  var modal = document.getElementById("myModal");
+  // // Get the modal
+  // var modal = document.getElementById("myModal");
   
-  // Get the button that opens the modal
-  var btn = document.getElementById("myBtn");
+  // // Get the button that opens the modal
+  // var btn = document.getElementById("myBtn");
   
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
+  // // Get the <span> element that closes the modal
+  // var span = document.getElementsByClassName("close")[0];
   
-  // When the user clicks the button, open the modal 
-  btn.onclick = function() {
-    modal.style.display = "block";
-  }
+  // // When the user clicks the button, open the modal 
+  // btn.onclick = function() {
+  //   modal.style.display = "block";
+  // }
   
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
-    modal.style.display = "none";
-  }
+  // // When the user clicks on <span> (x), close the modal
+  // span.onclick = function() {
+  //   modal.style.display = "none";
+  // }
   
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
+  // // When the user clicks anywhere outside of the modal, close it
+  // window.onclick = function(event) {
+  //   if (event.target == modal) {
+  //     modal.style.display = "none";
+  //   }
+  // }
 
   // send button back to mail page
+
 //   $(function(){
 //     $('#submit').click(function() {
 //         window.close();
 //         window.open('#masthead'); 
 //     });
+// // });
+
+// $(document).ready(function(){
+//     $('#submit').click(function() {
+//         $(".main-modal").hide("slow", function(){
+//         window.open('#masthead'); 
+//     });
+// });
 // });
 
+// H1 Typing Carousel
+
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 300 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+  document.body.appendChild(css);
+};
